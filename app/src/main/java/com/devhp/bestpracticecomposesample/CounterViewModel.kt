4ff39ee4fbd9cdbc0614ab8f0fc039acb1eaa7c1
vonguyenhongphuoc <vonguyenhongphuoc93@gmail.com>
2024-01-07 11:59:20 +0700

    Understand more about Flow

--- app/src/main/java/com/devhp/bestpracticecomposesample/CounterViewModel.kt
+++ app/src/main/java/com/devhp/bestpracticecomposesample/CounterViewModel.kt
@@ -1,17 +1,28 @@
 package com.devhp.bestpracticecomposesample
 
+import android.media.metrics.Event
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.collectIndexed
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.launch
 
 class CounterViewModel : ViewModel() {
     private val _screenState =
         mutableStateOf(MainScreenState(inputValue = "", displayResult = "Total is 0.0"))
     val screenState: State<MainScreenState> = _screenState
+
 
     private val _uiEventFlow = MutableSharedFlow<UIEvent>()
     val uiEventFlow = _uiEventFlow.asSharedFlow()
@@ -21,7 +32,11 @@ class CounterViewModel : ViewModel() {
     private fun addToTotal() {
         total += _screenState.value.inputValue.toDouble()
         _screenState.value =
-            _screenState.value.copy(displayResult = "Total is $total", isCountButtonVisible = false, inputValue = "")
+            _screenState.value.copy(
+                displayResult = "Total is $total",
+                isCountButtonVisible = false,
+                inputValue = ""
+            )
     }
 
     private fun resetTotal() {
@@ -61,5 +76,51 @@ class CounterViewModel : ViewModel() {
             }
         }
     }
+
+    private val eventInt = MutableSharedFlow<Int>()
+    private val channel = Channel<Int>()
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d("MyTag", "collect")
+            val last = channel.receive()
+            Log.d("last", last.toString())
+        }
+
+        Log.d("MyTag", "Init ViewModel")
+        viewModelScope.launch(Dispatchers.IO) {
+            for (i in 1..1000) {
+                Log.d("Emit", i.toString())
+                eventInt.emit(i)
+                if (i == 999) {
+                    channel.send(i)
+                    channel.close()
+                }
+            }
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val flow = flow {
+                emit(1)
+                delay(50)
+                emit(2)
+            }
+
+            flow.collect { value ->
+                println("AX Collecting $value")
+                delay(100) // Emulate work
+                println("AX $value collected")
+
+            }
+// Output: "Collecting 1", "1 collected", "Collecting 2", "2 collected"
+            println("AX.........................")
+            flow.collectLatest { value ->
+                println("AX Collecting $value")
+                delay(100) // Emulate work
+                println("AX $value collected")
+            }
+        }
+// Output: "Collecting 1", "Collecting 2", "2 collected"
+    }
+
 
 }
